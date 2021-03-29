c7c5185a2052b3bfd51c3db5d7668e40b4c49581
Clara Nascu <clarnasc082@ed.amdsb.ca>
2021-03-29 08:41:13 -0400

    Add project files.

---
+++ The_Collector/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace The_Collector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subTitleLabel = new System.Windows.Forms.Label();
+            this.winnerLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.easyButton = new System.Windows.Forms.Button();
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.timeLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLabel.Location = new System.Drawing.Point(12, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(143, 23);
+            this.timeLabel.TabIndex = 2;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(145, 92);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(342, 23);
+            this.titleLabel.TabIndex = 7;
+            this.titleLabel.Text = "+-+- THE COLLECTOR -+-+";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // subTitleLabel
+            // 
+            this.subTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTitleLabel.ForeColor = System.Drawing.Color.Green;
+            this.subTitleLabel.Location = new System.Drawing.Point(148, 178);
+            this.subTitleLabel.Name = "subTitleLabel";
+            this.subTitleLabel.Size = new System.Drawing.Size(348, 39);
+            this.subTitleLabel.TabIndex = 8;
+            this.subTitleLabel.Text = "Space to start, escape to exit";
+            this.subTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winnerLabel
+            // 
+            this.winnerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winnerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winnerLabel.ForeColor = System.Drawing.Color.Green;
+            this.winnerLabel.Location = new System.Drawing.Point(148, 133);
+            this.winnerLabel.Name = "winnerLabel";
+            this.winnerLabel.Size = new System.Drawing.Size(348, 39);
+            this.winnerLabel.TabIndex = 9;
+            this.winnerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.ForeColor = System.Drawing.Color.Green;
+            this.scoreLabel.Location = new System.Drawing.Point(12, 32);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(184, 48);
+            this.scoreLabel.TabIndex = 10;
+            // 
+            // easyButton
+            // 
+            this.easyButton.AutoSize = true;
+            this.easyButton.BackColor = System.Drawing.Color.Green;
+            this.easyButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.easyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.easyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.easyButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.easyButton.Location = new System.Drawing.Point(138, 245);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(96, 45);
+            this.easyButton.TabIndex = 12;
+            this.easyButton.Text = "easy";
+            this.easyButton.UseVisualStyleBackColor = false;
+            this.easyButton.Visible = false;
+            this.easyButton.Click += new System.EventHandler(this.easyButton_Click);
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.AutoSize = true;
+            this.mediumButton.BackColor = System.Drawing.Color.Olive;
+            this.mediumButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Yellow;
+            this.mediumButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mediumButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediumButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.mediumButton.Location = new System.Drawing.Point(270, 245);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(97, 45);
+            this.mediumButton.TabIndex = 13;
+            this.mediumButton.Text = "medium";
+            this.mediumButton.UseVisualStyleBackColor = false;
+            this.mediumButton.Visible = false;
+            this.mediumButton.Click += new System.EventHandler(this.mediumButton_Click);
+            // 
+            // hardButton
+            // 
+            this.hardButton.AutoSize = true;
+            this.hardButton.BackColor = System.Drawing.Color.Maroon;
+            this.hardButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.hardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hardButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hardButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.hardButton.Location = new System.Drawing.Point(405, 245);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(102, 45);
+            this.hardButton.TabIndex = 14;
+            this.hardButton.Text = "hard";
+            this.hardButton.UseVisualStyleBackColor = false;
+            this.hardButton.Visible = false;
+            this.hardButton.Click += new System.EventHandler(this.hardButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(700, 450);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.mediumButton);
+            this.Controls.Add(this.easyButton);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.winnerLabel);
+            this.Controls.Add(this.subTitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.timeLabel);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subTitleLabel;
+        private System.Windows.Forms.Label winnerLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button hardButton;
+    }
+}
+
---
+++ The_Collector/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace The_Collector
+{
+    public partial class Form1 : Form
+    {
+        string gameState = "waiting"; 
+
+        //ball
+        int ballX;
+        int ballY;
+        int ballSpeed;
+        int ballColour;
+        int ballSize = 10;
+        
+        //character
+        int characterX = 350;
+        int characterY = 400;
+        int characterWidth = 20;
+        int characterHeight = 20;
+        int characterXSpeed = 7;
+        int characterYSpeed = 7;
+
+        bool leftArrowDown = false;
+        bool rightArrowDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+        //invaders
+        List<int> invaderXList = new List<int>();
+        List<int> invaderYList = new List<int>();
+
+        List<int> invaderSpeedXList = new List<int>();
+        List<int> invaderSpeedYList = new List<int>();
+        List<string> invaderColourList = new List<string>();
+        int invaderWidth = 3;
+        int invaderHeight = 2;
+
+        List<int> astroidXList = new List<int>();
+        List<int> astroidYList = new List<int>();
+
+        List<int> astroidSpeedXList = new List<int>();
+        List<int> astroidSpeedYList = new List<int>();
+        List<string> astroidColourList = new List<string>(); /////////////////////////////////////////
+        int astroidWidth = 10;
+        int astroidHeight = 10;
+
+        string difficulty;  
+
+        Random randGen = new Random();
+        int randValue = 0;
+
+        int time = 600;
+        int score = 0;
+
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GameInitialize()
+        {
+            gameState = "level";
+            gameTimer.Enabled = true;
+
+            invaderXList.Clear();
+            invaderYList.Clear();
+
+            invaderSpeedXList.Clear();
+            invaderSpeedYList.Clear();
+            time = 600;
+            score = 0;
+            ballX = 50;
+            ballY = 200;
+            easyButton.Visible = true;
+            mediumButton.Visible = true;
+            hardButton.Visible = true;
+            winnerLabel.Text = " ";
+        }
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameState == "level")
+            {
+                titleLabel.Text = "+-+- THE COLLECTOR -+-+";
+                subTitleLabel.Text = "Choose your level...";
+                easyButton.Visible = true;
+                mediumButton.Visible = true;
+                hardButton.Visible = true;
+            }
+            else if (gameState == "waiting")
+            {
+                titleLabel.Text = "+-+- THE COLLECTOR -+-+";
+                subTitleLabel.Text = "Press Space Bar to Start or Escape to Exit";
+            }
+            else if (gameState == "running")
+            {
+                // draw text at top 
+                titleLabel.Text = $" ";
+                subTitleLabel.Text = $" ";
+                winnerLabel.Text = " ";
+                scoreLabel.Text = $"Score: {score}";
+                timeLabel.Text = $"Time left: {time}";
+
+                e.Graphics.FillRectangle(blueBrush, characterX, characterY, characterWidth, characterHeight);
+                e.Graphics.FillRectangle(greenBrush, ballX, ballY, ballSize, ballSize);
+
+                for (int i = 0; i < invaderYList.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, invaderXList[i], invaderYList[i], invaderWidth, invaderHeight);
+                }
+
+                for (int i = 0; i < astroidYList.Count(); i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, astroidXList[i], astroidYList[i], astroidWidth, astroidHeight);
+                }
+            }
+            else if (gameState == "over")
+            {
+                titleLabel.Text = "GAME OVER";
+                subTitleLabel.Text = "Press Space Bar to Start or Escape to Exit";
+                scoreLabel.Text = " ";
+            }
+            else if (gameState == "winner")
+            {
+                titleLabel.Text = "WINNER";
+                subTitleLabel.Text = "Press Space Bar to Start or Escape to Exit";
+                scoreLabel.Text = " ";
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Space:
+                    if (gameState == "waiting" || gameState == "over" || gameState == "winner")
+                    {
+                        GameInitialize();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameState == "waiting" || gameState == "over" || gameState == "winner")
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //Generate colored dot
+            Rectangle characterRec = new Rectangle(characterX, characterY, characterWidth, characterHeight);
+
+            for (int i = 0; i < invaderYList.Count; i++)
+            {
+                Rectangle ballRec = new Rectangle(ballX, ballY, ballSize, ballSize);
+                if (characterRec.IntersectsWith(ballRec))
+                {
+                    ballX = randGen.Next(20, this.Width - 50);
+                    ballY = randGen.Next(20, this.Height - 400);
+                    score = score + 1;
+                    SoundPlayer Player = new SoundPlayer(Properties.Resources._220173__gameaudio__spacey_1up_power_up);
+                    Player.Play();
+                }
+            }
+
+            //check to see if a new invader should be created            
+            randValue = randGen.Next(0, 101);
+            if (randValue < 12)
+            {
+                invaderYList.Add(randGen.Next(0, this.Height - 50));
+                invaderXList.Add(10);
+                invaderSpeedXList.Add(randGen.Next(2, 10));
+                invaderSpeedYList.Add(0);
+
+                if (randValue < 1)
+                {
+                    astroidYList.Add(randGen.Next(0, this.Height - 50));
+                    astroidXList.Add(10);
+                    astroidSpeedXList.Add(randGen.Next(10, 20));
+                    astroidSpeedYList.Add(0);
+                }
+
+                
+                if (difficulty == "medium" || difficulty == "hard")
+                {
+                    invaderYList.Add(randGen.Next(0, this.Height - 50));
+                    invaderXList.Add(660);
+                    invaderSpeedXList.Add(randGen.Next(-10, -2));
+                    invaderSpeedYList.Add(0);
+                }
+
+                if (difficulty == "hard")
+                {
+                    invaderXList.Add(randGen.Next(0, this.Width - 50));
+                    invaderYList.Add(10);
+                    invaderSpeedXList.Add(0);
+                    invaderSpeedYList.Add(randGen.Next(2, 10));
+                }
+
+                invaderColourList.Add("white");
+            }
+
+            for (int i = 0; i < invaderXList.Count(); i++)
+            {
+                invaderXList[i] += invaderSpeedXList[i];
+                invaderYList[i] += invaderSpeedYList[i];
+            }
+
+            for (int i = 0; i < astroidXList.Count(); i++)
+            {
+                astroidXList[i] += astroidSpeedXList[i];
+                astroidYList[i] += astroidSpeedYList[i];
+            }
+
+            //move character
+            if (upArrowDown == true && characterY > 0)
+            {
+                characterY -= characterYSpeed;
+            }
+
+            if (downArrowDown == true && characterY < this.Height - characterHeight)
+            {
+                characterY += characterYSpeed;
+            }
+
+            if (leftArrowDown == true && characterX > 0)
+            {
+                characterX -= characterXSpeed;
+            }
+
+            if (rightArrowDown == true && characterX < this.Width - characterWidth)
+            {
+                characterX += characterXSpeed;
+            }
+
+
+            //if character and invader collide..
+            for (int i = 0; i < invaderYList.Count; i++)
+            {
+                Rectangle invaderRec = new Rectangle(invaderXList[i], invaderYList[i], invaderWidth, invaderHeight);
+
+                if (characterRec.IntersectsWith(invaderRec))
+                {
+                    characterY = 400;
+                }
+            }
+
+            //if character and astroid collide..
+            for (int i = 0; i < astroidYList.Count; i++)
+            {
+                Rectangle astroidRec = new Rectangle(astroidXList[i], astroidYList[i], astroidWidth, astroidHeight);
+
+                if (characterRec.IntersectsWith(astroidRec))
+                {
+                    gameState = "over";
+                    timeLabel.Text = " ";
+                    SoundPlayer Player = new SoundPlayer(Properties.Resources._253886__themusicalnomad__negative_beeps__1_);
+                    Player.Play();
+                }
+            }
+
+            //decrease time counter and check to see if time is up 
+            time--;
+
+            if (time == 0)
+            {
+                gameTimer.Enabled = false;
+                gameState = "over";
+                characterY = 400;
+                characterX = 350;
+            }
+
+            //if player reaches a score of 3
+            if (score == 3)
+            {
+                gameState = "winner";
+                winnerLabel.Text = "Congratulations";
+                scoreLabel.Text = " ";
+                characterY = 400;
+                characterX = 350;
+            }
+
+            Refresh();
+        }
+
+        private void easyButton_Click(object sender, EventArgs e)
+        {
+            gameState = "running";
+            easyButton.Visible = false;
+            mediumButton.Visible = false;
+            hardButton.Visible = false;
+            time = 600;
+            SoundPlayer Player = new SoundPlayer(Properties.Resources._478337__joao_janz__bouncing_power_up_1_2);
+            Player.Play();
+            this.Focus();
+        }
+
+        private void mediumButton_Click(object sender, EventArgs e)
+        {
+            gameState = "running";
+            easyButton.Visible = false;
+            mediumButton.Visible = false;
+            hardButton.Visible = false;
+            difficulty = "medium";
+            time = 600;
+            SoundPlayer Player = new SoundPlayer(Properties.Resources._544886__mrthenoronha__twinkly_power_up);
+            Player.Play();
+            this.Focus();
+        }
+
+        private void hardButton_Click(object sender, EventArgs e)
+        {
+            gameState = "running";
+            easyButton.Visible = false;
+            mediumButton.Visible = false;
+            hardButton.Visible = false;
+            difficulty = "hard";
+            time = 600;
+            SoundPlayer Player = new SoundPlayer(Properties.Resources._511385__mrthenoronha__power_up_8_bit);
+            Player.Play();
+            this.Focus();
+        }
+    }
+}
