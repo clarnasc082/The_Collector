666c3c0da4f7569b55f8f3a29ab66fec52222f7b
Clara Nascu <clarnasc082@ed.amdsb.ca>
2021-04-06 13:39:39 -0400

    //changes

--- The_Collector/Properties/Resources.Designer.cs
+++ The_Collector/Properties/Resources.Designer.cs
@@ -146,26 +146,6 @@ namespace The_Collector.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Image1 {
-            get {
-                object obj = ResourceManager.GetObject("Image1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Image2 {
-            get {
-                object obj = ResourceManager.GetObject("Image2", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap rocket_icon {
             get {
                 object obj = ResourceManager.GetObject("rocket_icon", resourceCulture);
